ce39fc4fa6b8046453a787ebefaea3f1564c0663
Sarmad Rehan <sarmadrehan52@gmail.com>
2022-04-04 10:28:30 +0500

    Initial

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  Map<String, dynamic> example = {
+    'name': 'Sarmad Rehan',
+    'age': 21,
+    'profession': 'Student',
+  };
+  print(example["name"]);
+  print(example['age']);
+  print(example["profession"]);
+  print(example);
+}
