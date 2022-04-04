7ff326873729e56757c22ba19be59988b186f41c
Sarmad Rehan <sarmadrehan52@gmail.com>
2022-04-04 10:51:19 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,8 +4,22 @@ void main(List<String> args) {
     'age': 21,
     'profession': 'Student',
   };
-  print(example["name"]);
-  print(example['age']);
-  print(example["profession"]);
-  print(example);
+  // for (var key in example.keys) {
+  //   print(example[key]);
+  // }
+  // for (var value in example.values) {
+  //   print(value);
+  // }
+  // for (var entry in example.entries) {
+  //   print('${entry.key}:${entry.value}');
+  // }
+  Map<String, dynamic> example2 = {
+    'name': 'Rehan',
+    'age': 20,
+    'profession': 'Dart Developer',
+  };
+  List<Map> result = [];
+  result.add(example);
+  result.add(example2);
+  print(result);
 }
